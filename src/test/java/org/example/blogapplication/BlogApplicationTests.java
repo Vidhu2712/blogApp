a9d1b0fd625f78b2be421331197283e1ba26940f
vidhuu <vidhuu.singh.30.2@mountblue.tech>
2024-11-26 20:33:24 +0530

    final commit

--- src/test/java/org/example/blogapplication/BlogApplicationTests.java
+++ src/test/java/org/example/blogapplication/BlogApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.blogapplication;
 
+import io.mountblue.BlogApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BlogApplication.class)
 class BlogApplicationTests {
 
     @Test
